8a510a458a451648054e5a03645320ae42ac2c53
k-sakamoto3 <moi1990sk@gmail.com>
2016-10-12 07:11:39 +0900

    ソースコード修正

--- lib/hello_trema.rb
+++ lib/hello_trema.rb
@@ -1,10 +1,23 @@
 # Hello World!
 class HelloTrema < Trema::Controller
-  def start(args)
-    logger.info "Trema started (args = #{args.inspect})."
+
+  # クラス実行時
+  def start(_args)
+    # logger.info "Trema started (args = #{args.inspect})."
+    # logger.info "#{self.class} started." # => HelloTrema started.
+    logger.info "#{self.class.name} started." # => HelloTrema started.
   end
 
+  # 仮想スイッチが準備できた時
   def switch_ready(datapath_id)
-    logger.info format('Hello %#x!', datapath_id)
+    # logger.info format('Hello %#x!', datapath_id)
+    logger.info "Hello #{datapath_id.to_hex}!"
   end
+
+  # 仮想スイッチが切断された時
+  def switch_disconnected(datapath_id)
+    # logger.info format('Bye %#x', datapath_id)
+    logger.info "Bye #{datapath_id.to_hex}"
+  end
+
 end
